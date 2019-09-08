822e051a613483bedbaef75315603252d5716227
ekhossejn <45770055+ekhossejn@users.noreply.github.com>
2019-09-08 21:16:57 +0300

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -20,7 +20,7 @@ class MyStack():
         self.__data.append(int(element))
  
     def __repr__(self):
-        return 'MyStack({})'.format(', '.join(map(str, self.data)))
+        return 'MyStack({})'.format(', '.join(map(str, self.__data)))
  
     def get_data(self) -> list:
         return self.__data[:]
@@ -50,41 +50,42 @@ def test_top():
         None
     else:
         raise Exception('Failed')
-    a.push(3)
-    assert(a.top() == 3)
-    a.push(4)
-    assert(a.top() == 4)
-    a.pop()
-    assert(a.top() == 3)
-    a.pop()
+    for i in range(10):
+        a.push(i)
+        assert(a.top() == i)
+    for i in range(10):
+        assert(a.top() == (9 - i))
+        a.pop()
  
 def test_pop():
-    a.push(3)
-    a.push(4)
-    assert(a.pop() == 4)
-    assert(a.pop() == 3)
     try:
         a.pop()
     except IndexError:
         None
     else:
         raise Exception('Failed')
+    for i in range(10):
+        a.push(i)
+    for i in range(10):
+        assert(a.pop() == (9 - i))
  
 def test_push():
-    try:
-        a.push(3)
-    except TypeError:
-        raise Exception('Failed')
-    else:
-        None
-    try:
-        a.push('3')
-    except TypeError:
-        None
-    else:
-        raise Exception('Failed')
-    a.pop()
- 
+    for i in range(5):
+        try:
+            a.push(str(i))
+        except TypeError:
+            None
+        else:
+            raise Exception('Failed')
+    for i in range(5):
+        try:
+            a.push(i)
+        except TypeError:
+            raise Exception('Failed')
+        else:
+            None
+    for i in range(5):
+        a.pop()
 def test_data():
     assert(a.get_data() == [])
     a.push(2)
@@ -97,7 +98,22 @@ def test_data():
     arr = a.get_data()
     arr.pop(-1)
     assert(a.get_data() == [2, 3, 4])
- 
+    a.pop()
+    a.pop()
+    a.pop()
+
+def test_repr():
+    assert(a.__repr__() == 'MyStack()')
+    a.push(2)
+    a.push(3)
+    a.push(4)
+    a.push(2)
+    assert(a.__repr__() == 'MyStack(2, 3, 4, 2)')
+    a.pop()
+    assert(a.__repr__() == 'MyStack(2, 3, 4)')
+    arr = a.__repr__()
+    arr = 'MyStack()'
+    assert(a.__repr__() == 'MyStack(2, 3, 4)')
 a = MyStack()
 test_is_empty()
 test_size()
@@ -105,3 +121,4 @@ test_top()
 test_pop()
 test_push()
 test_data()
+test_repr()
